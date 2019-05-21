fc24b6400b13871883e127364015a5feb2e0d9bf
Soo Yeon Shim <sooyshim@gmail.com>
2019-05-20 20:54:40 -0400

    fixed media query

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const paintings = [
     url: "./assets/cranachJudgement.jpg",
     author: "Lucas Cranach the Elder",
     alt:
-      "In the foreground, from left to right, there are a horse, a seated young man wearing armour and a crimson cape, an old man standing and holding a glass orb, three women in nude. One of the women wears a large hat and transparent drapery. In the background, a putto is shooting an arrow.",
-    metUrl:
-      "https://www.metmuseum.org/art/collection/search/436037?&searchField=All&sortBy=Date&deptids=11&high=on&ft=*&offset=0&rpp=80&amp;pos=23"
+      "In the foreground, from left to right, there are a horse, a seated young man wearing armour and a crimson cape, an old man standing and holding a glass orb, three women in nude. One of the women wears a large hat and transparent drapery. In the background, a putto is shooting an arrow."
   },
   {
     title: "Venus and Adonis",
@@ -18,9 +16,7 @@ const paintings = [
     url: "./assets/titianVenus.jpg",
     author: "Titian",
     alt:
-      "A woman, depicted in a colorist manner, in nude is seated and turned her back towards us. She holds a man who is departing. The man hold two hunting dogs, and a cupid on the left is looking over the scene.",
-    metUrl:
-      "https://www.metmuseum.org/art/collection/search/437826?&searchField=All&sortBy=Date&deptids=11&high=on&ft=*&offset=0&rpp=80&amp;pos=26"
+      "A woman, depicted in a colorist manner, in nude is seated and turned her back towards us. She holds a man who is departing. The man hold two hunting dogs, and a cupid on the left is looking over the scene."
   },
   {
     title: "The Harvesters",
@@ -29,9 +25,7 @@ const paintings = [
     url: "./assets/brugelElderHarvesters.jpg",
     author: "Pieter Brugel the Elder",
     alt:
-      "A group of men and women are resting under the tree, while some men are harvesting in a golden field.",
-    metUrl:
-      "https://www.metmuseum.org/art/collection/search/435809?&searchField=All&sortBy=Date&deptids=11&high=on&ft=*&offset=0&rpp=80&amp;pos=27"
+      "A group of men and women are resting under the tree, while some men are harvesting in a golden field."
   },
   {
     title: "Juan de Pareja (1601-1670)",
@@ -40,9 +34,7 @@ const paintings = [
     url: "./assets/velazquezJuan.jpg",
     author: "Velázquez",
     alt:
-      "A portrait of an artist's assistant, gazing intensely out the pictorial space, depicted in a high degree of versimilitude.",
-    urlMet:
-      "https://www.metmuseum.org/art/collection/search/437869?&searchField=All&sortBy=Date&deptids=11&high=on&ft=*&offset=0&rpp=80&amp;pos=46"
+      "A portrait of an artist's assistant, gazing intensely out the pictorial space, depicted in a high degree of versimilitude."
   },
   {
     title: "Young Woman with a Water Pitcher",
@@ -51,9 +43,7 @@ const paintings = [
     url: "./assets/vermeerYoung.jpg",
     author: "Johannes Vermeer",
     alt:
-      "A young woman in an interior with a water pitcher, standing beside a window lit by sun light.",
-    metUrl:
-      "https://www.metmuseum.org/art/collection/search/437881?&searchField=All&sortBy=Date&deptids=11&high=on&ft=*&offset=0&rpp=80&amp;pos=49"
+      "A young woman in an interior with a water pitcher, standing beside a window lit by sun light."
   },
   {
     title: "Self-Portrait",
@@ -62,9 +52,7 @@ const paintings = [
     url: "./assets/rembrandtSelf.jpg",
     author: "Rembrandt",
     alt:
-      "A self-portrait of an artist in his fifties. The artist depicts his face in a realist manner, showing the signs of aging.",
-    metUrl:
-      "https://www.metmuseum.org/art/collection/search/437881?&searchField=All&sortBy=Date&deptids=11&high=on&ft=*&offset=0&rpp=80&amp;pos=49"
+      "A self-portrait of an artist in his fifties. The artist depicts his face in a realist manner, showing the signs of aging."
   },
   {
     title: "Mezzetin",
@@ -73,9 +61,7 @@ const paintings = [
     url: "./assets/watteauMezzetin.jpg",
     author: "Antoine Watteau",
     alt:
-      "Mezzetin seated and singing in a garden. The Mezzetin's smooth silk and the trees in the garden are rendered with soft and delicate brush touches.",
-    metUrl:
-      "https://www.metmuseum.org/art/collection/search/437926?&searchField=All&sortBy=Date&deptids=11&high=on&ft=*&offset=0&rpp=80&amp;pos=56"
+      "Mezzetin seated and singing in a garden. The Mezzetin's smooth silk and the trees in the garden are rendered with soft and delicate brush touches."
   },
   {
     title: "Manuel Osorio Manrique de Zuñiga (1784-1792)",
@@ -83,9 +69,7 @@ const paintings = [
     century: "18th century",
     url: "./assets/goyaManuel.jpg",
     author: "Goya",
-    alt: "A boy wearing a red costume with a magpie, finches, and three cats.",
-    metUrl:
-      "https://www.metmuseum.org/art/collection/search/436545?&searchField=All&sortBy=Date&deptids=11&high=on&ft=*&offset=0&rpp=80&amp;pos=65"
+    alt: "A boy wearing a red costume with a magpie, finches, and three cats."
   },
   {
     title: "Venice, from the Porch of Madonna della Salute",
@@ -94,9 +78,7 @@ const paintings = [
     url: "./assets/turnerVenice.jpg",
     author: "Joseph Mallord William Turner",
     alt:
-      "A landscape painting of Venice, from the Porch of Madonna della Salute. The reflections of the boats on the water are depicted.",
-    metUrl:
-      "https://www.metmuseum.org/art/collection/search/437853?&searchField=All&sortBy=Date&deptids=11&high=on&ft=*&offset=0&rpp=80&amp;pos=71"
+      "A landscape painting of Venice, from the Porch of Madonna della Salute. The reflections of the boats on the water are depicted."
   },
   {
     title: "The Dance Class",
@@ -105,9 +87,7 @@ const paintings = [
     url: "./assets/degasDance.jpg",
     author: "Edgar Degas",
     alt:
-      "In the foreground, young ballerinas who seem to be done with her performance are standing, in the middle ground, a group of ballerinas is performing in front of an old man, in the back ground, another group is waiting for its turn. ",
-    metUrl:
-      "https://www.metmuseum.org/art/collection/search/438817?&searchField=All&sortBy=Date&deptids=11&high=on&ft=*&offset=80&rpp=80&amp;pos=88"
+      "In the foreground, young ballerinas who seem to be done with her performance are standing, in the middle ground, a group of ballerinas is performing in front of an old man, in the back ground, another group is waiting for its turn. "
   },
   {
     title: "Self-Portrait with a Straw Hat (obverse: The Potato Peeler)",
@@ -116,9 +96,7 @@ const paintings = [
     url: "./assets/goghSelf.jpg",
     author: "Vincent van Gogh",
     alt:
-      "A self-portrait of an artist wearing a straw hat. The artist uses crude, thick, slab-like brush strokes.",
-    metUrl:
-      "https://www.metmuseum.org/art/collection/search/436532?&searchField=All&sortBy=Date&deptids=11&high=on&ft=*&offset=80&rpp=80&amp;pos=94"
+      "A self-portrait of an artist wearing a straw hat. The artist uses crude, thick, slab-like brush strokes."
   },
   {
     title: "Mäda Primavesi (1903-2000)",
@@ -127,9 +105,7 @@ const paintings = [
     url: "./assets/klimtMada.jpg",
     author: "Gustav Klimt",
     alt:
-      "a portrait of a young girl wearing a white dress, decorated with a belt of flowers. The pink background and a floral field give a decorative impression.",
-    metUrl:
-      "https://www.metmuseum.org/art/collection/search/436819?&searchField=All&sortBy=Date&deptids=11&high=on&ft=*&offset=80&rpp=80&amp;pos=104"
+      "a portrait of a young girl wearing a white dress, decorated with a belt of flowers. The pink background and a floral field give a decorative impression."
   }
 ];
 
@@ -504,7 +480,6 @@ quizApp.reviewAnswers = paintings => {
       const $paintingAuthor = $(`<p>${paintings[i].author}</p>`);
       const $paintingCountry = $(`<p>${paintings[i].country}</p>`);
       const $paintingCentury = $(`<p>${paintings[i].century}</p>`);
-      const $paintingUrl = $(`<a href="${paintings[i].metUrl}">Read more</a>`);
       const $userInputs = $(
         `<div class="userInputs"><p class="userAnswers">Your answers:</p></div>`
       );
@@ -514,8 +489,7 @@ quizApp.reviewAnswers = paintings => {
         $paintingTitle,
         $paintingAuthor,
         $paintingCountry,
-        $paintingCentury,
-        $paintingUrl
+        $paintingCentury
       );
       $userInputs.append($userCountry, $userCentury);
       $reviewContents = $(`<div class="reviewContents">`);
