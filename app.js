8410c64d6e7924f64c88d2c16398c0d8758b4f1d
sooyshim <sooyshim@gmail.com>
2019-07-10 17:44:19 -0400

    modified styles and added sweetalert

--- app.js
+++ app.js
@@ -1,116 +1,9 @@
-// Paintings data pool
-const paintings = [
-  {
-    title: "The Judgement of Paris",
-    country: "Germany",
-    century: "16th century",
-    url: "./assets/cranachJudgement.jpg",
-    author: "Lucas Cranach the Elder",
-    alt:
-      "In the foreground, from left to right, there are a horse, a seated young man wearing armour and a crimson cape, an old man standing and holding a glass orb, three women in nude. One of the women wears a large hat and transparent drapery. In the background, a putto is shooting an arrow."
-  },
-  {
-    title: "Venus and Adonis",
-    country: "Italy",
-    century: "16th century",
-    url: "./assets/titianVenus.jpg",
-    author: "Titian",
-    alt:
-      "A woman, depicted in a colorist manner, in nude is seated and turned her back towards us. She holds a man who is departing. The man hold two hunting dogs, and a cupid on the left is looking over the scene."
-  },
-  {
-    title: "The Harvesters",
-    country: "Belgium",
-    century: "16th century",
-    url: "./assets/brugelElderHarvesters.jpg",
-    author: "Pieter Brugel the Elder",
-    alt:
-      "A group of men and women are resting under the tree, while some men are harvesting in a golden field."
-  },
-  {
-    title: "Juan de Pareja (1601-1670)",
-    country: "Spain",
-    century: "17th century",
-    url: "./assets/velazquezJuan.jpg",
-    author: "Velázquez",
-    alt:
-      "A portrait of an artist's assistant, gazing intensely out the pictorial space, depicted in a high degree of versimilitude."
-  },
-  {
-    title: "Young Woman with a Water Pitcher",
-    country: "Netherlands",
-    century: "17th century",
-    url: "./assets/vermeerYoung.jpg",
-    author: "Johannes Vermeer",
-    alt:
-      "A young woman in an interior with a water pitcher, standing beside a window lit by sun light."
-  },
-  {
-    title: "Self-Portrait",
-    country: "Netherlands",
-    century: "17th century",
-    url: "./assets/rembrandtSelf.jpg",
-    author: "Rembrandt",
-    alt:
-      "A self-portrait of an artist in his fifties. The artist depicts his face in a realist manner, showing the signs of aging."
-  },
-  {
-    title: "Mezzetin",
-    country: "France",
-    century: "18th century",
-    url: "./assets/watteauMezzetin.jpg",
-    author: "Antoine Watteau",
-    alt:
-      "Mezzetin seated and singing in a garden. The Mezzetin's smooth silk and the trees in the garden are rendered with soft and delicate brush touches."
-  },
-  {
-    title: "Manuel Osorio Manrique de Zuñiga (1784-1792)",
-    country: "Spain",
-    century: "18th century",
-    url: "./assets/goyaManuel.jpg",
-    author: "Goya",
-    alt: "A boy wearing a red costume with a magpie, finches, and three cats."
-  },
-  {
-    title: "Venice, from the Porch of Madonna della Salute",
-    country: "England",
-    century: "19th century",
-    url: "./assets/turnerVenice.jpg",
-    author: "Joseph Mallord William Turner",
-    alt:
-      "A landscape painting of Venice, from the Porch of Madonna della Salute. The reflections of the boats on the water are depicted."
-  },
-  {
-    title: "The Dance Class",
-    country: "France",
-    century: "19th century",
-    url: "./assets/degasDance.jpg",
-    author: "Edgar Degas",
-    alt:
-      "In the foreground, young ballerinas who seem to be done with her performance are standing, in the middle ground, a group of ballerinas is performing in front of an old man, in the back ground, another group is waiting for its turn. "
-  },
-  {
-    title: "Self-Portrait with a Straw Hat (obverse: The Potato Peeler)",
-    country: "France",
-    century: "19th century",
-    url: "./assets/goghSelf.jpg",
-    author: "Vincent van Gogh",
-    alt:
-      "A self-portrait of an artist wearing a straw hat. The artist uses crude, thick, slab-like brush strokes."
-  },
-  {
-    title: "Mäda Primavesi (1903-2000)",
-    country: "Austria",
-    century: "20th century",
-    url: "./assets/klimtMada.jpg",
-    author: "Gustav Klimt",
-    alt:
-      "a portrait of a young girl wearing a white dress, decorated with a belt of flowers. The pink background and a floral field give a decorative impression."
-  }
-];
+import { artworks } from "./paintings.js";
 
-// Declaring the app
+// Name-spacing 
 const quizApp = {};
+
+const paintings = artworks;
 
 // --------------------------------------------------------------------------------
 //function group: create new arrays needed for generating a quiz from the paintings array
@@ -204,10 +97,10 @@ quizApp.paintingInQuestion = (paintings, index) => {
 
 //function: update the DOM with three choices
 quizApp.populateQuizOptions = (answerOptions, inputName, domElement) => {
-  for (i = 0; i < answerOptions.length; i++) {
+  for (let i = 0; i < answerOptions.length; i++) {
     const $answerOptions = $(`
     <div className="optionContainer">
-      <input type="radio" id="${inputName}${i}" tabindex="0" name=${inputName} class=${inputName} value="${
+      <input type="radio" id="${inputName}${i}" name=${inputName} class=${inputName} value="${
       answerOptions[i]
     }"/>
       <label for="${inputName}${i}">${answerOptions[i]}</label>
@@ -292,7 +185,7 @@ quizApp.init = () => {
 quizApp.startQuiz = () => {
   $(".start").on("click", function() {
     // Hide the header and display the quiz section
-    $(".headerContent").addClass("hide");
+    $(".headerContent").css("display", "none");
     $(".navReset")
       .removeClass("hide")
       .addClass("show");
@@ -325,14 +218,13 @@ quizApp.playQuiz = () => {
 
     //error handling: prevent unchecked inputs from submitting
     if (!$countryChoice.is(":checked") || !$centuryChoice.is(":checked")) {
-      // Swal.fire({
-      //   title: 'Error!',
-      //   text: 'Do you want to continue',
-      //   type: 'error',
-      //   confirmButtonText: 'Cool'
-      // });
-      alert("Please select your answers");
-      return false;
+      Swal.fire({
+        title: 'Please select your answers!',
+        text: 'You must choose a country and a century',
+        type: 'error',
+        confirmButtonText: 'OK'
+      });
+      return false
     }
 
     //get user inputs
@@ -423,7 +315,8 @@ quizApp.endQuiz = () => {
     .addClass("hide");
   $(".resultSection")
     .removeClass("hide")
-    .addClass("show");
+    .addClass("show")
+    .css("height", "80vh");
   //display view score button
   $(".viewScore")
     .removeClass("hide")
@@ -470,14 +363,13 @@ quizApp.viewScore = () => {
 quizApp.reviewAnswers = paintings => {
   $(".reviewAnswers").on("click", function() {
     $(".resultSection")
-      .removeClass("show")
-      .addClass("hide");
+      .css("display", "none");
     $(".reviewSection")
       .removeClass("hide")
       .addClass("show");
 
     //update the DOM with quized paintings
-    for (i = 0; i < paintings.length; i++) {
+    for (let i = 0; i < paintings.length; i++) {
       const $galleryItem = $(`<li class="galleryItem" tabindex=0>`);
       const $paintingImg = $(`<img>`).attr("src", paintings[i].url);
       $paintingImg.attr("alt", "");
@@ -498,7 +390,7 @@ quizApp.reviewAnswers = paintings => {
         $paintingCentury
       );
       $userInputs.append($userCountry, $userCentury);
-      $reviewContents = $(`<div class="reviewContents">`);
+      const $reviewContents = $(`<div class="reviewContents">`);
       $reviewContents.append($paintingInfo, $userInputs);
       $galleryItem.append($paintingImg, $reviewContents);
       $(".gallery").append($galleryItem);
---
+++ paintings.js
@@ -0,0 +1,110 @@
+export const artworks = [
+    {
+        title: "The Judgement of Paris",
+        country: "Germany",
+        century: "16th century",
+        url: "./assets/cranachJudgement.jpg",
+        author: "Lucas Cranach the Elder",
+        alt:
+            "In the foreground, from left to right, there are a horse, a seated young man wearing armour and a crimson cape, an old man standing and holding a glass orb, three women in nude. One of the women wears a large hat and transparent drapery. In the background, a putto is shooting an arrow."
+    },
+    {
+        title: "Venus and Adonis",
+        country: "Italy",
+        century: "16th century",
+        url: "./assets/titianVenus.jpg",
+        author: "Titian",
+        alt:
+            "A woman, depicted in a colorist manner, in nude is seated and turned her back towards us. She holds a man who is departing. The man hold two hunting dogs, and a cupid on the left is looking over the scene."
+    },
+    {
+        title: "The Harvesters",
+        country: "Belgium",
+        century: "16th century",
+        url: "./assets/brugelElderHarvesters.jpg",
+        author: "Pieter Brugel the Elder",
+        alt:
+            "A group of men and women are resting under the tree, while some men are harvesting in a golden field."
+    },
+    {
+        title: "Juan de Pareja (1601-1670)",
+        country: "Spain",
+        century: "17th century",
+        url: "./assets/velazquezJuan.jpg",
+        author: "Velázquez",
+        alt:
+            "A portrait of an artist's assistant, gazing intensely out the pictorial space, depicted in a high degree of versimilitude."
+    },
+    {
+        title: "Young Woman with a Water Pitcher",
+        country: "Netherlands",
+        century: "17th century",
+        url: "./assets/vermeerYoung.jpg",
+        author: "Johannes Vermeer",
+        alt:
+            "A young woman in an interior with a water pitcher, standing beside a window lit by sun light."
+    },
+    {
+        title: "Self-Portrait",
+        country: "Netherlands",
+        century: "17th century",
+        url: "./assets/rembrandtSelf.jpg",
+        author: "Rembrandt",
+        alt:
+            "A self-portrait of an artist in his fifties. The artist depicts his face in a realist manner, showing the signs of aging."
+    },
+    {
+        title: "Mezzetin",
+        country: "France",
+        century: "18th century",
+        url: "./assets/watteauMezzetin.jpg",
+        author: "Antoine Watteau",
+        alt:
+            "Mezzetin seated and singing in a garden. The Mezzetin's smooth silk and the trees in the garden are rendered with soft and delicate brush touches."
+    },
+    {
+        title: "Manuel Osorio Manrique de Zuñiga (1784-1792)",
+        country: "Spain",
+        century: "18th century",
+        url: "./assets/goyaManuel.jpg",
+        author: "Goya",
+        alt: "A boy wearing a red costume with a magpie, finches, and three cats."
+    },
+    {
+        title: "Venice, from the Porch of Madonna della Salute",
+        country: "England",
+        century: "19th century",
+        url: "./assets/turnerVenice.jpg",
+        author: "Joseph Mallord William Turner",
+        alt:
+            "A landscape painting of Venice, from the Porch of Madonna della Salute. The reflections of the boats on the water are depicted."
+    },
+    {
+        title: "The Dance Class",
+        country: "France",
+        century: "19th century",
+        url: "./assets/degasDance.jpg",
+        author: "Edgar Degas",
+        alt:
+            "In the foreground, young ballerinas who seem to be done with her performance are standing, in the middle ground, a group of ballerinas is performing in front of an old man, in the back ground, another group is waiting for its turn. "
+    },
+    {
+        title: "Self-Portrait with a Straw Hat (obverse: The Potato Peeler)",
+        country: "France",
+        century: "19th century",
+        url: "./assets/goghSelf.jpg",
+        author: "Vincent van Gogh",
+        alt:
+            "A self-portrait of an artist wearing a straw hat. The artist uses crude, thick, slab-like brush strokes."
+    },
+    {
+        title: "Mäda Primavesi (1903-2000)",
+        country: "Austria",
+        century: "20th century",
+        url: "./assets/klimtMada.jpg",
+        author: "Gustav Klimt",
+        alt:
+            "a portrait of a young girl wearing a white dress, decorated with a belt of flowers. The pink background and a floral field give a decorative impression."
+    }
+];
+
